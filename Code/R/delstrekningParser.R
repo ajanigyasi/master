3c3c585ae5e9d402e02eff37692056b35fe55b11
Emil Lunde <emil.oien.lunde@gmail.com>
2015-01-29 11:14:21 +0100

    added r script for parsing delstrekning files

---
+++ Code/R/delstrekningParser.R
@@ -0,0 +1,7 @@
+travels <- read.csv("~/dev/master/Data/20150128_reiser_test.csv", sep=";", stringsAsFactor = FALSE)
+start <- paste(travels$startdato, travels$starttid, sep = " ")
+start <- strptime(start, "%Y-%m-%d %H:%M:%S")
+end <- paste(travels$sluttdato, travels$sluttid, sep = " ")
+end <- strptime(end, "%Y-%m-%d %H:%M:%S")
+time <- difftime(end, start, unit = "sec")
+travels <- data.frame(travels$delstrekning_id, start, end, time)
