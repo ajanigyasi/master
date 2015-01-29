40871e409f71920c4880eab40d9ef188984ada7e
Thomas Wolff <t>
2015-01-29 15:24:22 +0100

    Wrote the script using nested while-loops instead of using the function "by"

--- Code/R/deriveTravelTimesFromPointRegistrations.R
+++ Code/R/deriveTravelTimesFromPointRegistrations.R
@@ -1,7 +1,7 @@
 # Read data
-travels <<- read.csv("../../Data/20150128_passeringer_test.csv", stringsAsFactors=FALSE, sep=";")
+travels <- read.csv("../../Data/20150128_passeringer_test.csv", stringsAsFactors=FALSE, sep=";")
 # Order data by tag id
-travels <<- travels[order(travels[,3]),]
+travels <- travels[order(travels[,3]),]
 # Extract time and date
 dateAndTime <- paste(travels$dato, travels$tid, sep = " ")
 dateAndTime <- strptime(dateAndTime, "%Y-%m-%d %H:%M:%S")
@@ -9,10 +9,31 @@ dateAndTime <- strptime(dateAndTime, "%Y-%m-%d %H:%M:%S")
 travels$dateAndTime = dateAndTime
 # Delete dato column and tid column
 drops = c("dato", "tid")
-travels <<- travels[, !(names(travels) %in% drops)]
+travels <- travels[, !(names(travels) %in% drops)]
 # Insert column for travel times
 travels$travelTime = rep(NA,1,dim(travels)[1])
-travels$rowNumber = 1:dim(travels)[1]
+# travels$rowNumber = 1:dim(travels)[1]
+
+# Start the clock!
+ptm <- proc.time()
+
+i = 1
+currentTagId = travels[i, c("brikke_id")]
+
+while(i < dim(travels)[1]){
+  j = i+1
+  nextTagId = travels[j, c("brikke_id")]
+  while(currentTagId == nextTagId && j < dim(travels)[1]){
+    travels[j-1, c("travelTime")] <- difftime(travels[j, c("dateAndTime")], travels[j-1, c("dateAndTime")], unit = "sec")
+    j = j+1
+    nextTagId = travels[j, c("brikke_id")]
+  }
+  i = j
+  currentTagId = travels[i, c("brikke_id")]
+}
+
+# Stop the clock
+print(proc.time() - ptm)
 
 # Helper functions
 calculateTravelTimesForTagId <- function(rowsForTagId){
@@ -25,11 +46,7 @@ calculateTravelTimesForTagId <- function(rowsForTagId){
 }
 
 
-# Start the clock!
-ptm <- proc.time()
+
 
 # Calculate travel times
-by(data=travels,INDICES=travels$brikke_id,FUN=calculateTravelTimesForTagId)
-
-# Stop the clock
-print(proc.time() - ptm)
+# by(data=travels,INDICES=travels$brikke_id,FUN=calculateTravelTimesForTagId)
