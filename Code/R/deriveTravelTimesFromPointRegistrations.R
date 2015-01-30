616ddaa25ffd45ccfdf388d406cde776b84b32e5
Thomas Wolff <t>
2015-01-30 10:09:30 +0100

    Added new data files, updated travel time script

--- Code/R/deriveTravelTimesFromPointRegistrations.R
+++ Code/R/deriveTravelTimesFromPointRegistrations.R
@@ -1,22 +1,33 @@
+# Take in file name as argument
+args <- commandArgs(trailingOnly = TRUE)
+
+# Extract file name
+fullFileName = args[1]
+len = nchar(fullFileName)
+fileExt = substr(fullFileName, start=len-3, len)
+fileName = substr(fullFileName, start=1, stop=len-4)
+
 # Read data
-travels <- read.csv("../../Data/20150128_passeringer_test.csv", stringsAsFactors=FALSE, sep=";")
+travels <- read.csv(fullFileName, stringsAsFactors=FALSE, sep=";")
+
 # Order data by tag id
-travels <- travels[order(travels[,3]),]
+travels <- travels[order(travels[,c("brikke_id")]),]
+
 # Extract time and date
 dateAndTime <- paste(travels$dato, travels$tid, sep = " ")
 dateAndTime <- strptime(dateAndTime, "%Y-%m-%d %H:%M:%S")
+
 # Insert time and date column
 travels$dateAndTime = dateAndTime
+
 # Delete dato column and tid column
 drops = c("dato", "tid")
 travels <- travels[, !(names(travels) %in% drops)]
+
 # Insert column for travel times
 travels$travelTime = rep(NA,1,dim(travels)[1])
-# travels$rowNumber = 1:dim(travels)[1]
 
-# Start the clock!
-ptm <- proc.time()
-
+# Calculate travel times
 i = 1
 currentTagId = travels[i, c("brikke_id")]
 
@@ -32,21 +43,5 @@ while(i < dim(travels)[1]){
   currentTagId = travels[i, c("brikke_id")]
 }
 
-# Stop the clock
-print(proc.time() - ptm)
-
-# Helper functions
-calculateTravelTimesForTagId <- function(rowsForTagId){
-  # For each row in the input
-  for (i in 1:(dim(rowsForTagId)[1]-1)){
-    #   Calculate the difference with the next row
-    #   Insert the travel time
-    travels[rowsForTagId[i, c("rowNumber")], c("travelTime")] <<- difftime(rowsForTagId[i+1, c("dateAndTime")], rowsForTagId[i, c("dateAndTime")], unit = "sec")
-  }
-}
-
-
-
-
-# Calculate travel times
-# by(data=travels,INDICES=travels$brikke_id,FUN=calculateTravelTimesForTagId)
+# Write results to file
+write.table(travels, paste(fileName, "_med_reisetider", fileExt), sep=";", row.names=FALSE)
