acec95e3d6c2a0ca1acc0477745ee7d43aae749a
Emil Lunde <emil.oien.lunde@gmail.com>
2015-01-30 09:31:22 +0100

    delstrekningParser now saves data frame

--- Code/R/delstrekningParser.R
+++ Code/R/delstrekningParser.R
@@ -1,9 +1,20 @@
-args <- commandArgs(trailingOnly = TRUE)
+args <- commandArgs(trailingOnly = TRUE) #get command line arguments
 
-travels <- read.csv(args[1], sep=";", stringsAsFactor = FALSE)
-start <- paste(travels$startdato, travels$starttid, sep = " ")
+travels <- read.csv(args[1], sep=";", stringsAsFactor = FALSE) #create data frame from csv file
+
+#create date objects
+start <- paste(travels$startdato, travels$starttid, sep = " ") 
 start <- strptime(start, "%Y-%m-%d %H:%M:%S")
 end <- paste(travels$sluttdato, travels$sluttid, sep = " ")
 end <- strptime(end, "%Y-%m-%d %H:%M:%S")
+
+#calculate travel time
 time <- difftime(end, start, unit = "sec")
+
+#create new data frame with desired atttributes
 travels <- data.frame(travels$delstrekning_id, start, end, time)
+
+#save data frame
+save(travels, file = "travels.Rda")
+#save(travels, file = "travels.Rda", compress = "xz")
+#write.table(travels, file = "travels.csv", sep = ";", row.names = FALSE)
