0754f74585e3e302b65b1c8b15747dfda600eac0
Emil Lunde <emil.oien.lunde@gmail.com>
2015-02-09 14:43:00 +0100

    svm.R tests if we are able to predict travels times with svm, while lasso.R tests if we are able to combine to prediction models using lasso (unfinished)

---
+++ Code/R/svm.R
@@ -0,0 +1,25 @@
+library(caret)
+library(kernlab) #needed for svm
+
+#Read data 
+klett_samf_jan14 = read.csv2("../../Data/O3-H-01-2014/klett_samf_jan14.csv")
+
+#Extract travel times and construct trainingdata
+traveltimes = klett_samf_jan14$Reell.reisetid..sek.
+l = length(traveltimes)
+y = traveltimes[-1:-2]
+x1 = traveltimes[2:(l-1)]
+x2 = traveltimes[1:(l-2)]
+data = as.data.frame(cbind(x1, x2, y))
+
+#Partition data into training and testing sets
+trainingindices = unlist(createDataPartition(1:8926, p=0.7))
+trainingdata = data[trainingindices, ]
+testingdata = data[-trainingindices, 1:2]
+targetTraveltimes = data[-trainingindices, 3]
+
+#Train SVM
+svm = train(y~x1+x2, trainingdata, method="svmLinear")
+
+predictedTravelTimes = predict(svm, testingdata)
+diff = abs(predictedTravelTimes - targetTravelTimes)
