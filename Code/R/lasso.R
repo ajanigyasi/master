48d7b797f94cbfe4fdf69be9558a9592af4f0fdf
Emil Lunde <emil.oien.lunde@gmail.com>
2015-02-10 09:57:46 +0100

    lasso.R uses lasso to combine to models. just for testing purposes

--- Code/R/lasso.R
+++ Code/R/lasso.R
@@ -1,10 +1,11 @@
 library(caret)
 library(kernlab) #needed for svm
+library(elasticnet) #needed for lasso
 
-#Read data 
+#read data 
 klett_samf_jan14 = read.csv2("../../Data/O3-H-01-2014/klett_samf_jan14.csv")
 
-#Extract travel times and construct trainingdata
+#extract travel times and construct trainingdata
 traveltimes = klett_samf_jan14$Reell.reisetid..sek.
 l = length(traveltimes)
 y = traveltimes[-1:-2]
@@ -12,13 +13,27 @@ x1 = traveltimes[2:(l-1)]
 x2 = traveltimes[1:(l-2)]
 data = as.data.frame(cbind(x1, x2, y))
 
-#Partition data into training and testing sets
+#partition data into training and testing sets
 trainingindices = unlist(createDataPartition(1:8926, p=0.7))
 trainingdata = data[trainingindices, ]
 testingdata = data[-trainingindices, 1:2]
-targetTravelTimes = data[-trainingindices, 3]
 
-#Train SVM
-svm = train(y~x1+x2, trainingdata, method="svmLinear")
+#train SVM
+svm <- train(y~x1+x2, trainingdata, method="svmLinear")
 
-#Train kNN
+#train kNN
+knn <- knnreg(trainingdata[, 1:2], trainingdata[, 3])
+
+#create dataframe of predictions from both models
+svmPredictions <- predict(svm, testingdata)
+knnPredictions <- predict(knn, testingdata)
+predictions <- as.matrix(cbind(svmPredictions, knnPredictions))
+
+#create vector of the correct travel times
+response <- data[-trainingindices, 3]
+
+#create lasso model based on the predictions and correct travel times
+lasso <- train(predictions, response, method="lasso") #setting lambda=0 performs lasso fit
+
+#use lasso model to predict
+lassoPredictions <- predict(lasso, predictions) #we predict the same data we trained on, don't do this!
