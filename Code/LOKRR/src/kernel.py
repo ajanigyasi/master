86adac2a9f2672720a8f4cf2e33b6cc175517859
Emil Lunde <emil.oien.lunde@gmail.com>
2015-02-27 11:20:49 +0100

    implmented constructor and predict function

--- Code/LOKRR/src/kernel.py
+++ Code/LOKRR/src/kernel.py
@@ -1,5 +1,5 @@
-from numpy.core import exp
-from scipy.spatial.distance import pdist, squareform
+from numpy import asmatrix, exp, identity, add, dot, repeat, reshape
+from scipy.spatial.distance  import pdist, cdist, squareform
 
 
 class kernel:
@@ -7,16 +7,21 @@ class kernel:
     """
     Constructor
     Parameters:
-        data - numpy matrix containing input variables and corresponding output
+        x - numpy matrix containing input variables
+        y - vector containing corresponding output
         s - float representing sigma used in radial basis function
         l - float representing lambda used in kernel ridge regression
     """ 
-    def __init__(self, data, s, l):
-        pairwise_sq_dists = squareform(pdist(data, 'sqeuclidean'))
-        self.K = exp(-pairwise_sq_dists / (2*(s**2)))
+    def __init__(self, X, y, s, l):
+        self.X = X
+        self.y = y
         self.s = s
         self.l = l
-        
+        pairwise_sq_dists = squareform(pdist(X, 'sqeuclidean'))
+        K = asmatrix(exp(-pairwise_sq_dists / (2*(s**2)))) #gaussian RBF
+        I = identity(K.shape[0])
+        self.reg_K = add(K, l*I) #K + lambda*I
+        self.reg_K_inv = self.reg_K.getI() #inverse of (K + lambda*I)
             
     def updateK(self, data_point):
         None
@@ -29,4 +34,9 @@ class kernel:
         self.updateKInv()
     
     def predict(self, data_point):
-        None
+        tmp = dot(self.y, self.reg_K_inv)
+        foo = cdist(asmatrix(data_point), self.X)
+        foo = asmatrix(foo).getT().shape
+        print tmp.shape
+        return dot(tmp, foo)
+        
--- Code/LOKRR/src/lokrr.py
+++ Code/LOKRR/src/lokrr.py
@@ -1,8 +1,7 @@
 from numpy import matrix
 from src.kernel import kernel
 if __name__ == '__main__':
-    matrix = matrix('1 2; 3 4; 5 6')
-    kernel = kernel(matrix, 1 ,1)
-    print kernel.K
-    
+    matrix = matrix('1 2; 3 4')
+    kernel = kernel(matrix, [5, 6], 1 ,3)
+    print kernel.predict([1, 1])
     
