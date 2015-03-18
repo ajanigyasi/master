fe8869b8bec2ea28aa178fc5ca86e61d0132376f
Thomas Wolff <t>
2015-03-18 09:58:50 +0100

    Updated the script to store the data set in increasing order of when the vehicle entered the road section to ease plotting.

--- Code/R/createDataSet.R
+++ Code/R/createDataSet.R
@@ -17,7 +17,7 @@ passeringerFileName2 = args[4]
 delstrekningId = args[5]
 
 # Get date
-fullFileName = reiserFileName1
+fullFileName = reiserFileName2
 len = nchar(reiserFileName2)
 fileExt = substr(fullFileName, start=len-3, len)
 fileName = substr(fullFileName, start=1, len-26)
@@ -30,7 +30,7 @@ travelTimes2 <- read.csv(reiserFileName2, stringsAsFactors=FALSE, sep=";")
 passages1 <- read.csv(passeringerFileName1, stringsAsFactors=FALSE, sep=";")
 passages2 <- read.csv(passeringerFileName2, stringsAsFactors=FALSE, sep=";")
 
-# Select a delstrekning
+# Select a specific road section
 travelTimes1 <- travelTimes1[travelTimes1$travels.delstrekning_id==delstrekningId, ]
 travelTimes2 <- travelTimes2[travelTimes2$travels.delstrekning_id==delstrekningId, ]
 travelTimes <- rbind(travelTimes1, travelTimes2)
@@ -39,42 +39,48 @@ passages1 <- passages1[passages1$antenne_id==10091, ]
 passages2 <- passages2[passages2$antenne_id==10091, ]
 passages <- rbind(passages1, passages2)
 
+# Order travelTimes and passages in increasing date and time of when the vehicle entered the road section
 passages <- passages[order(passages[,c("dateAndTime")]),]
+travelTimes <- travelTimes[order(travelTimes[,c("start")]),]
 
-# Convert date and time to dateTime objects
+# Convert date and time to POSIXlt objects
 travelTimes$start <- strptime(travelTimes$start, "%Y-%m-%d %H:%M:%S")
 travelTimes$end <- strptime(travelTimes$end, "%Y-%m-%d %H:%M:%S")
-
 passages$dateAndTime <- strptime(passages$dateAndTime, "%Y-%m-%d %H:%M:%S")
 
-getLastFiveMinutesEnd <- function(rowNum){
+# Function for retrieving the rows for the last five minutes, given a row number
+getRowsForLastFiveMinutes <- function(rowNum){
   currentTime = travelTimes[rowNum, c("start")]
   return(travelTimes[(travelTimes$end<=currentTime)&(travelTimes$end>=(currentTime-300)),])
 }
 
-getLastFiveMinutesStart <- function(rowNum){
+# Function for retrieving the number of rows in the last five minutes, given a row number
+getNumberOfRowsForLastFiveMinutes <- function(rowNum){
   currentTime = travelTimes[rowNum, c("start")]
   return(nrow(passages[((passages$dateAndTime<=currentTime)&(passages$dateAndTime>=(currentTime-300))),]))
 }
 
-# Calculate 5 min mean travel times for each row
+# Get number of rows in travel times data frame
 n = dim(travelTimes)[1]
-dataSet = data.frame(cbind(travelTimes$time, rep(300, n), rep(0, n)))
-colnames(dataSet) = c("actualTravelTime", "fiveMinuteMean", "trafficVolume")
 
+# Initialize data set
+dataSet = data.frame(cbind(travelTimes$start), rep(300, n), rep(0, n), travelTimes$time)
+colnames(dataSet) = c("dateAndTime", "fiveMinuteMean", "trafficVolume", "actualTravelTime")
+
+# Compute five minute mean travel times and traffic volume
 print("Computing five minute means and traffic volumes...")
 for (i in 1:n){
-  prevRows = getLastFiveMinutesEnd(i)
+  prevRows = getRowsForLastFiveMinutes(i)
   if(nrow(prevRows)>=1){
     dataSet[i, c("fiveMinuteMean")] = mean(prevRows$time)
   }
-  dataSet[i, c("trafficVolume")] = getLastFiveMinutesStart(i)
+  dataSet[i, c("trafficVolume")] = getNumberOfRowsForLastFiveMinutes(i)
   cat("\r", round((i/n)*100, 1), "%", sep="")
   flush.console()
 }
 cat("\r\n")
 
 # Write data set to file
-# The data set is stored in increasing date and time for when the vehicle exited the road section
+# The data set is stored in increasing date and time for when the vehicle entered the road section
 write.table(dataSet, paste(fileName, "_dataset", fileExt, sep=""), sep=";", row.names=FALSE)
 print("createDataSet completed without errors")
