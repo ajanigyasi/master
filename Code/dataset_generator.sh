f4da82cfa80b568a42aa1012d59e2fa401e25168
Thomas Wolff <thomaswolff90@gmail.com>
2015-03-25 14:30:47 +0000

    Added data set files

--- Code/dataset_generator.sh
+++ Code/dataset_generator.sh
@@ -3,6 +3,7 @@
 #change directory to the directory where this script is located
 cd ${0%/*}
 
+Rscript R/createDataSet.R 20150315 2015 03 16
 Rscript R/createDataSet.R 20150316 2015 03 17 
 Rscript R/createDataSet.R 20150317 2015 03 18 
 Rscript R/createDataSet.R 20150318 2015 03 19 
