6f04fb27ff74e5b5e725a369d06be4a6e5bd73a3
Thomas Wolff <t>
2015-04-10 09:27:14 +0200

    Added python file for reading data

--- Code/Python/AdaBoostTest.py
+++ Code/Python/AdaBoostTest.py
@@ -1,35 +1,49 @@
 # Libraries
 import numpy as np
 import scipy
+from datetime import *
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import AdaBoostRegressor
+from sklearn import neighbors
+from dataSetGetter import getDataSet
 
-# Import dataset
-travelTimes = np.loadtxt('../../Data/20150128_reiser_med_reisetider.csv', usecols=[3], delimiter=';',skiprows=1)
-x1 = np.array(travelTimes[:111335])
-x2 = np.array(travelTimes[1:111336])
-X = scipy.transpose(np.matrix([x1,x2]))
-y = travelTimes[2:111337]
+# Initialize start date, end date, directory and model
+startDate = datetime.strptime("20150129", '%Y%m%d')
+endDate = datetime.strptime("20150130", '%Y%m%d')
+directory = "../../Data/Autopassdata/Singledatefiles/Dataset/raw/"
+model = "dataset"
+
+# Read data set
+dataSet = getDataSet(startDate, endDate, directory, model)
 
 # Build weak learners based on data
-weakLearner = DecisionTreeRegressor(max_depth=4)
-weakLearner.fit(X, y)
+#weakLearner = DecisionTreeRegressor(max_depth=4)
+#weakLearner.fit(X, y)
+
+# Build knn model based on data
+x1 = dataSet['fiveMinuteMean']
+x2 = dataSet['trafficVolume']
+X = [x1, x2]
+print(X.shape)
+print(X[1])
 
 # Build boosted learners
-boostedLearner = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4), n_estimators=300, random_state=np.random.RandomState(1))
-boostedLearner.fit(X, y)
+#boostedLearner = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4), n_estimators=300, random_state=np.random.RandomState(1))
+#boostedLearner.fit(X, y)
 
 # Do prediction
-y1 = weakLearner.predict(X)
-y2 = boostedLearner.predict(X)
+#y1 = weakLearner.predict(X)
+#y2 = boostedLearner.predict(X)
 
 # Plot difference between the weak learners, and the boosted learner
-plt.figure()
-plt.plot([i for i in range(1, 111336)], y1, c='g', label='n_estimators=1', linewidth=2)
-plt.plot([i for i in range(1, 111336)], y2, c='r', label='n_estimators=300', linewidth=2)
-plt.xlabel('data')
-plt.ylabel('target')
-plt.title('Boosted Decision Tree Regression')
-plt.legend()
-plt.show()
+#plt.figure()
+#plt.plot([i for i in range(1, 111336)], y1, c='g', label='n_estimators=1', linewidth=2)
+#plt.plot([i for i in range(1, 111336)], y2, c='r', label='n_estimators=300', linewidth=2)
+#plt.xlabel('data')
+#plt.ylabel('target')
+#plt.title('Boosted Decision Tree Regression')
+#plt.legend()
+#plt.show()
+
+a = raw_input("Done...")
