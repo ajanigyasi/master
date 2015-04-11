a7b5200930e6e2bb47b940156041acbb85a21c88
Emil Lunde <emil.oien.lunde@gmail.com>
2015-04-11 12:07:39 +0200

    Finished implementing lokrr constructor.

--- Code/Python/lokrr.py
+++ Code/Python/lokrr.py
@@ -1,4 +1,4 @@
-from numpy import matrix, loadtxt, vstack, asmatrix, where, asarray, zeros, hstack
+from numpy import matrix, loadtxt, vstack, asmatrix, where, asarray, zeros, hstack, delete
 from kernel import kernel
 from utils import getDataSet
 from datetime import time, date, datetime, timedelta
@@ -9,55 +9,41 @@ class lokrr:
         self.data = data
         self.window_size = window_size
         self.kernel_map = dict()
-        print self.kernel_map
         self.create_kernels(self.data, self.window_size, self.kernel_map)
 
-    def get_list_of_times(self, list_of_datetimes):
-        list_of_times = list()
-        for dt in list_of_datetimes:
-            list_of_times.append(dt.time())
-        return asarray(list_of_times)
-
+    #Creates a data set based on the list of indices passed as an argument
     def create_dataset(self, data, indices):
         fiveMinuteMean = data['fiveMinuteMean'][indices]
         trafficVolume = data['trafficVolume'][indices]
         actualTravelTime = data['actualTravelTime'][indices]
         return vstack((fiveMinuteMean, trafficVolume, actualTravelTime)).T
 
-
+    #Creates kernels based on the data for every 5-minute interval of the day
+    #and adds them to kernel_map
     def create_kernels(self, data, window_size, kernel_map):
         intervals = get_list_of_intervals(datetime(2015, 1, 1, 0, 0, 0),
                                           datetime(2015, 1, 1, 23, 59, 59), timedelta(minutes=5))
-        list_of_times = self.get_list_of_times(data['dateAndTime'])
+        intervals.append(datetime(2015, 1, 1, 23, 59, 59))
+        list_of_times = get_list_of_times(data['dateAndTime'])
         list_of_datasets = list()
-        for i in range(0, len(intervals)-1):
+        for i in range(0, len(intervals)-1): #create data set for every 5-minute interval
             indices = where((list_of_times > intervals[i].time()) &
                              (list_of_times < intervals[i+1].time()))
             dataset = self.create_dataset(self.data, indices)
             list_of_datasets.append(dataset)
-        #for i in range(0, len(list_of_datasets)):
-        for i in range(8,9): #TODO: REMOVE!
+        for i in range(0, len(list_of_datasets)): #combine data sets with neighbors
             kernel_data = zeros((1, 3))
             for j in range(window_size, 0, -1):
                 if ((i - j) >= 0):
-                    print kernel_data.shape
-                    print list_of_datasets[i-j].shape
                     kernel_data = vstack((kernel_data, list_of_datasets[i-j]))
+            kernel_data = vstack((kernel_data, list_of_datasets[i]))
             for j in range(1, window_size+1):
                 if ((i + j) <= len(list_of_datasets)-1):
-                    print list_of_datasets[i+j].shape
                     kernel_data = vstack((kernel_data, list_of_datasets[i+j]))
-            print kernel_data.shape
-            #create kernel
-            #add kernel to kernel_map
-        
-
-            #TODO: add each kernel to kernel_map
-        #     k = kernel(dataset[:, 0:1], dataset[:,2], 1, 1)
-        # print k.X
+            kernel_data = delete(kernel_data, 0, axis=0) #delete first row
+            k = kernel(kernel_data[:, [0,1]], kernel_data[:, 2], 1, 1) #create kernel
+            kernel_map[str(intervals[i].time())] = k #add kernel to kernel_map
             
-        #indices = where((list_of_times > intervals[0].time()) & (list_of_times < intervals[1].time()))
-        None
 
     def predict(self, data_point):
         None
@@ -65,14 +51,24 @@ class lokrr:
     def update(self, data_point):
         None
 
+#Returns a list containing datetime objects between start and end with delta
+#distance between them
 def get_list_of_intervals(start, end, delta):
     intervals = list()
     curr = start
     while curr < end:
         intervals.append(curr)
         curr += delta
-    intervals.append(datetime(2015, 1, 1, 23, 59, 59))
     return intervals
+
+#Returns an array containing the time (without the date)
+#for each datetime object in list_of_datetimes
+def get_list_of_times(list_of_datetimes):
+    list_of_times = list()
+    for dt in list_of_datetimes:
+        list_of_times.append(dt.time())
+    return asarray(list_of_times)
+
 
 if __name__ == '__main__':
     from_date = "20150219"
@@ -80,7 +76,8 @@ if __name__ == '__main__':
     dir = "../../Data/Autopassdata/Singledatefiles/Dataset/raw/"
     model = "dataset"
     dataset = getDataSet(from_date, to_date, dir, model)
-    lokrr(dataset, 1)
+    l = lokrr(dataset, 3)
+    print l.kernel_map
     
     # intervals = get_list_of_intervals(datetime(2015, 1, 1, 0, 0, 0), datetime(2015, 1, 1, 23, 59, 59), timedelta(minutes=5))
     # indices = where((dataset['dateAndTime'] > intervals[0]) & (dataset['dateAndTime'] < intervals[1]))
@@ -104,14 +101,6 @@ if __name__ == '__main__':
     
     #normalize dataset
     #in article: data for each kernel is normalized seperately
-
-    X = asmatrix(vstack((dataset['fiveMinuteMean'], dataset['trafficVolume']))).T
-    y = dataset['actualTravelTime']
-    X = X[0:2, : ]
-    y = y[0:2]
-    k = kernel(X, y, 1, 1)
-
-    
     
     # print k.reg_K_inv
     # print k.reg_K
