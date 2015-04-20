d65c979b8c1abe1d337214a0766198d99940cb2b
Thomas Wolff <thomaswolff90@gmail.com>
2015-04-20 13:49:19 +0000

    Added results of training final knn model

--- Code/R/knn_parameter_tuner.R
+++ Code/R/knn_parameter_tuner.R
@@ -8,9 +8,9 @@ preProcess <- function(data, column) {
   normalize(data[column][, 1], minimum, maximum)
 }
 
-startDate <- "20150129"
-endDate <- "20150204"
-splitDate <- "20150202"
+startDate <- "20150205"
+endDate <- "20150225"
+#splitDate <- "20150202"
 directory <- "../../Data/Autopassdata/Singledatefiles/Dataset/"
 
 # Get data set from startDate to endDate
@@ -19,16 +19,14 @@ dataSet <- getDataSet(startDate, endDate, paste(directory, "raw/", sep=""), 'fil
 #normalize data and partition into training and testing set
 dataSet$fiveMinuteMean <- preProcess(dataSet, "fiveMinuteMean")
 dataSet$trafficVolume <- preProcess(dataSet, "trafficVolume")
-splitIndex <- which(dataSet$dateAndTime >= as.Date(c(splitDate), "%Y%m%d"))[1]
-trainingSet <- dataSet[1:(splitIndex-1), ]
-testingSet <- dataSet[splitIndex:nrow(dataSet), ]
-trainingSet$actualTravelTime <- preProcess(trainingSet, "actualTravelTime")
+dataSet$actualTravelTime <- preProcess(dataSet, "actualTravelTime")
+#splitIndex <- which(dataSet$dateAndTime >= as.Date(c(splitDate), "%Y%m%d"))[1]
+#trainingSet <- dataSet[1:(splitIndex-1), ]
+#testingSet <- dataSet[splitIndex:nrow(dataSet), ]
 
 formula <- actualTravelTime~fiveMinuteMean+trafficVolume
-ctrl <- trainControl(verboseIter = TRUE, , method='cv')
+ctrl <- trainControl(verboseIter = TRUE, method='none')
 
-knn_grid <- expand.grid(kmax = c(3, 5, 7, 10, 20, 50), distance = c(1, 2), kernel = 
-                          c("rectangular", "optimal", "triangular", "epanechnikov", "triweight",
-                            "cos", "inv", "gaussian", "rank", "optimal"))
-knnMod = train(formula, trainingSet, method="kknn", trControl = ctrl, tuneGrid = knn_grid)
-save(knnMod, file="new_baselines/knnMod.RData")
+knn_grid <- expand.grid(kmax = c(50), distance = c(1), kernel=c("rank"))
+knnMod = train(formula, dataSet, method="kknn", trControl = ctrl, tuneGrid = knn_grid)
+save(knnMod, file="new_baselines/main_knnMod.RData")
